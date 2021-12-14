5ef82f4ec10d6f8048f65f8182bd194a87090f52
Dwayne Sutherland <DwayneaSutherland1@gmail.com>
2021-12-14 12:33:06 +0000

    setup communication between api and sql server

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,17 +1,17 @@
 module.exports = {
   development: {
     storage: "./db-dev.sqlite3",
-    dialect: "sqlite"
+    dialect: "sqlite",
   },
   test: {
     storage: ":memory:",
-    dialect: "sqlite"
+    dialect: "sqlite",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
-    dialect: "mysql"
-  }
+    dialect: "mysql",
+  },
 };
