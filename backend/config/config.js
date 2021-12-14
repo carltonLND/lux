9c5e9be8da0656ace1e911b6061b9cc423a509a4
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2021-12-14 10:45:01 +0000

    updated production config

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,11 +3,15 @@ module.exports = {
     storage: "./db-dev.sqlite3",
     dialect: "sqlite"
   },
-  test:
-    storage: ":memory",
+  test: {
+    storage: ":memory:",
     dialect: "sqlite"
   },
   production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
     dialect: "mysql"
   }
 };
