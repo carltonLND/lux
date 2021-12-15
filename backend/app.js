b13c82ab3f0b3e418c7b8f5790f6f11198cd9b1e
Carlton (CJ) <carltonlnd@hotmail.com>
2021-12-15 14:13:06 +0000

    removed old test router

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-// var indexRouter = require('./routes/index');
 // var usersRouter = require("./routes/users");
-var testRouter = require("./routes/test")
 
 var app = express();
 
@@ -15,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
 // app.use("/users", usersRouter);
-app.use("/test", testRouter);
 
 module.exports = app;
