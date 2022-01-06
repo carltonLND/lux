f96e7b52c61b618122557409cea7322b849ef53a
Carlton (CJ) <carltonlnd@hotmail.com>
2022-01-06 20:16:09 +0000

    added specific origin to cors

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// set cors for specific origin
-app.use(cors());
+app.use(cors({ origin: "lux.midasdesign.co.uk" }));
 
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
