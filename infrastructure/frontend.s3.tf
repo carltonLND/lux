173f1207dea5dada5413820d3fd3ffbbdf184b2c
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-07-16 18:23:53 +0100

    created frontend infrastructure

--- infrastructure/frontend.s3.tf
+++ infrastructure/frontend.s3.tf
@@ -21,6 +21,6 @@ resource "aws_s3_bucket_website_configuration" "static-site" {
 
 resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
   bucket = aws_s3_bucket.static-site.bucket
-  policy = templatefile("bucket_policy.tftpl", {bucket_arn = aws_s3_bucket.static-site.arn, oai_id = aws_cloudfront_origin_access_identity.OAI.id})
+  policy = templatefile("frontend.bucket_policy.tftpl", {bucket_arn = aws_s3_bucket.static-site.arn, oai_id = aws_cloudfront_origin_access_identity.OAI.id})
 
 }
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" { 
+  region = var.region
 }
 
 provider "aws" {
   alias = "us-east"
-
   region = "us-east-1"
 }
