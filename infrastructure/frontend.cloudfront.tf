b64fcc1da0faf52a7e527ee5066b975d9e5e840d
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-08-12 09:26:24 +0100

    update npm build step

--- infrastructure/frontend.cloudfront.tf
+++ infrastructure/frontend.cloudfront.tf
@@ -58,7 +58,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    cloudfront_default_certificate = false
     acm_certificate_arn            = aws_acm_certificate.ssl.arn
     ssl_support_method             = "sni-only"
   }
